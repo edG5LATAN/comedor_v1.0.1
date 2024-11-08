e16d81c77a85979ee7b399e018955ba6aaac4b6c
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2024-11-08 09:37:19 -0600

    Update Bebidas.jsx
    lista bebidas

--- src/component/bebidas/Bebidas.jsx
+++ src/component/bebidas/Bebidas.jsx
@@ -5,35 +5,35 @@ import './Bebidas.css'
 
 function Bebidas() {
   
-  const [bebidas, setbebidas] = useState([
+   const [bebidas, setbebidas] = useState([
     {
-      bebida: "amburgueza gigante",
+      bebida: "cola sin soda",
       precio: "40$",
       imagen: "https://i.pinimg.com/originals/1a/77/ab/1a77abcb412e7ac22b262efbb539172e.jpg",
     },
     {
-      bebida: "amburgueza peque;a",
+      bebida: "gigirell",
       precio: "90$",
       imagen: "https://i.pinimg.com/originals/01/31/b4/0131b48bf26545ab2a07f1c5b56f54ed.jpg",
     },
     {
-      bebida: "amburgueza media",
+      bebida: "malteada",
       precio: "70$",
       imagen: "https://i.pinimg.com/originals/56/e3/f5/56e3f5ca472d3f20693a42433ad88cec.jpg",
     },
     {
-      bebida: "amburgueza suiza",
+      bebida: "ramsperry",
       precio: "60$",
       imagen: "https://i.pinimg.com/originals/56/ac/77/56ac77eb9e518b4449857b5dc532c89d.jpg",
     },
     {
-      bebida: "amburgueza alemana",
+      bebida: "cerveza alemana",
       precio: "25$",
       imagen: "https://i.pinimg.com/originals/33/8e/b4/338eb4a04cf20fd03498c12b2d3d68b1.jpg",
     },
     ,
     {
-      bebida: "amburgueza ",
+      bebida: "cerveza barril ",
       precio: "15$",
       imagen: "https://i.pinimg.com/originals/fa/74/6b/fa746b34f3bd2ee1ad59e1899a349e61.jpg",
     }
